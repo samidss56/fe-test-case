739f1152040e5fee7eb8b78964355ad074038b0e
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-04-28 15:05:40 +0700

    Implement GET endpoint to return predefined todo items

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server';
+import { TodoType } from '~/types/todo';
+
+const todos: TodoType[] = Array.from({ length: 3 }, (_, i) => {
+  const id = i + 1;
+  return { id, title: `Todo ${id}`, description: `Todo ${id} description` };
+});
 
 export async function GET() {
   return NextResponse.json({
-    data: [],
+    data: todos,
   });
 }
