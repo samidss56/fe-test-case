c43d184d1d4d07e565c807adc32d695860fb1df7
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-05-07 13:35:09 +0700

    refactor: remove unused data.json and update utils for empty todos handling

--- src/app/api/todos/utils.ts
+++ src/app/api/todos/utils.ts
@@ -1,9 +1,14 @@
+import { existsSync } from 'fs';
 import { readFile, writeFile } from 'fs/promises';
 import { TodoType } from '~/types/todo';
 
 const path = process.cwd() + '/src/app/api/todos/data.json';
 
 export async function getTodos(): Promise<TodoType[]> {
+  if (!existsSync(path)) {
+    await replaceTodos([]);
+  }
+
   const file = await readFile(path, 'utf-8');
   return JSON.parse(file);
 }
